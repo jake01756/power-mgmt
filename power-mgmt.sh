ab66ec8f77a55e611e2bf4b74f86ef5307afe5e2
Jake Thomas <28026633+jake01756@users.noreply.github.com>
2022-01-01 22:44:39 -0500

    Inital Commit

---
+++ power-mgmt.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#echo Welcome to power-mgmt!
+
+helpFunction()
+{
+   figlet power-mgmt -l -f slant
+   echo ""
+   echo "Usage: power-mgmt --action [PERFORMABLE ACTION] [--force level] -c parameterC"
+   echo -e "\t--action - Describes what action to perform."
+   echo -e "\t--force [level] - Force the action to happen using the specified severity level."
+  # echo -e "\t-c Description of what is parameterC"
+   echo "" 
+   echo "Performable Actions:"
+   echo -e "\t s, shutdown - Shutdown the system gracefully"
+   echo -e "\t r, restart - Reboot the system gracefully"
+   #echo -e ""
+   echo ""
+   echo "Force Levels:"
+   echo -e "\t 0 - (default) The system will notify all users and services that the system is shutting down."
+   echo -e "\t 1 - The system will shutdown without notifying users and shutting down services. Everything is killed and disks are unmounted before the system is shutdown."
+   echo -e "\t 2 - systemctl will manage the shutdown without notifying systemd."
+exit 1 # Exit script after printing help
+}
+confirmShutdown()
+{
+   #
+   
+
+   echo "The system is shutting down NOW"
+}
+
+confirmForcedShutdow()
+{
+   echo "The system is shutting down forcefully NOW"
+}
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterA" ] || [ -z "$parameterB" ] || [ -z "$parameterC" ]
+then
+   #echo "Some or all of the required parameters are empty";
+   #helpFunction
+fi
+
+case $1 in
+   
+
+
+
+if [ $1 == "" ]
+then
+   echo "A required parameter was missing."
+   echo "Please see the -h command."
+   exit 1
+fi
+
+# Begin script in case all parameters are correct
+echo "$parameterA"
+echo "$parameterB"
+echo "$parameterC"
+echo "$2"
